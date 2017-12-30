445a93bb21f88ca80564afc6d40afe78bcd2e00c
rinnan <rinnan@u.washington.edu>
2017-12-30 14:23:54 -0800

    Updated example data.

---
+++ R/ABPR.R
@@ -0,0 +1,8 @@
+#' Range map for Abies procera
+#'
+#' Historical range map for Abies procera, from "Atlas of United States Trees" by Elbert L. Little, Jr.
+#'
+#' @format A SpatialPolygonDataFrame.
+#' @source \url{www.usgs.gov}
+#' @family example data
+"ABPR"
