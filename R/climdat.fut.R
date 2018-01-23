41e0224dfec00226815cfe5788271f189f3ebe48
rinnan <rinnan@u.washington.edu>
2018-01-22 20:37:38 -0800

    Updated climate datasets,

--- R/climdat.fut.R
+++ R/climdat.fut.R
@@ -1,31 +1,29 @@
-#' Future climate data for \emph{Abies procera}
+#' Future climate data
 #'
-#' Future climate dataset of 19 bioclimate variables (\url{www.worldclim.org}),
-#' cropped to the extent of the historical occurrence of the noble pine
-#' (\emph{Abies procera}). Based on the MIROC5 GCM projections under the RCP8.5
-#' scenario for 2050.
+#' Future climate dataset of 10 bioclimate variables (\url{www.worldclim.org}).
+#' Based on the MIROC5 GCM projections under the RCP8.5 scenario for 2050.
 #'
-#' @format A RasterBrick with 19 layers.
+#' @format A RasterBrick with 10 layers:
 #' \describe{
-#'   \item{MAT}{mean annual temperature (degrees C * 10)}
+#   \item{MAT}{mean annual temperature (degrees C * 10)}
 #'   \item{MDR}{mean diurnal range (mean of monthly max temp - min temp)}
 #'   \item{ISO}{isothermality (MDR/TAR * 100)}
 #'   \item{TS}{temperature seasonality (sd monthly temp * 100)}
 #'   \item{HMmax}{max temp of warmest month}
 #'   \item{CMmin}{min temp of coldest month}
-#'   \item{TAR}{temp annual range (HMmax - CMmin)}
-#'   \item{MTWQ}{mean temp of wettest quarter}
-#'   \item{MTDQ}{mean temp of driest quarter}
-#'   \item{MTHQ}{mean temp of hottest quarter}
-#'   \item{MTCQ}{mean temp of coldest quarter}
-#'   \item{TAP}{total annual precipitation (mm)}
+#   \item{TAR}{temp annual range (HMmax - CMmin)}
+#   \item{MTWQ}{mean temp of wettest quarter}
+#   \item{MTDQ}{mean temp of driest quarter}
+#   \item{MTHQ}{mean temp of hottest quarter}
+#   \item{MTCQ}{mean temp of coldest quarter}
+#   \item{TAP}{total annual precipitation (mm)}
 #'   \item{PWM}{precip of wettest month}
 #'   \item{PDM}{precip of driest month}
 #'   \item{PS}{precip seasonality (sd/mean monthly precip)}
 #'   \item{PWQ}{precip of wettest quarter}
 #'   \item{PDQ}{precip of driest quarter}
-#'   \item{PHQ}{precip of hottest quarter}
-#'   \item{PCQ}{precip of coldest quarter}
+#   \item{PHQ}{precip of hottest quarter}
+#   \item{PCQ}{precip of coldest quarter}
 #' }
 #' @source \url{www.worldclim.org}
 #' @seealso \code{\link{climdat.hist}}, \code{\link{tree-data}}
--- R/climdat.hist.R
+++ R/climdat.hist.R
@@ -1,30 +1,28 @@
-#' Historical climate data for \emph{Abies procera}
+#' Historical climate data
 #'
-#' Historical climate dataset of 19 bioclimate variables (\url{www.worldclim.org}),
-#' cropped to the extent of the historical occurrence of the noble pine
-#' (\emph{Abies procera}).
+#' Historical climate dataset of 10 bioclimate variables (\url{www.worldclim.org}).
 #'
-#' @format A RasterBrick with 19 layers.
+#' @format A RasterBrick with 10 layers:
 #' \describe{
-#'   \item{MAT}{mean annual temperature (degrees C * 10)}
+#   \item{MAT}{mean annual temperature (degrees C * 10)}
 #'   \item{MDR}{mean diurnal range (mean of monthly max temp - min temp)}
 #'   \item{ISO}{isothermality (MDR/TAR * 100)}
 #'   \item{TS}{temperature seasonality (sd monthly temp * 100)}
 #'   \item{HMmax}{max temp of warmest month}
 #'   \item{CMmin}{min temp of coldest month}
-#'   \item{TAR}{temp annual range (HMmax - CMmin)}
-#'   \item{MTWQ}{mean temp of wettest quarter}
-#'   \item{MTDQ}{mean temp of driest quarter}
-#'   \item{MTHQ}{mean temp of hottest quarter}
-#'   \item{MTCQ}{mean temp of coldest quarter}
-#'   \item{TAP}{total annual precipitation (mm)}
+#   \item{TAR}{temp annual range (HMmax - CMmin)}
+#   \item{MTWQ}{mean temp of wettest quarter}
+#   \item{MTDQ}{mean temp of driest quarter}
+#   \item{MTHQ}{mean temp of hottest quarter}
+#   \item{MTCQ}{mean temp of coldest quarter}
+#   \item{TAP}{total annual precipitation (mm)}
 #'   \item{PWM}{precip of wettest month}
 #'   \item{PDM}{precip of driest month}
 #'   \item{PS}{precip seasonality (sd/mean monthly precip)}
 #'   \item{PWQ}{precip of wettest quarter}
 #'   \item{PDQ}{precip of driest quarter}
-#'   \item{PHQ}{precip of hottest quarter}
-#'   \item{PCQ}{precip of coldest quarter}
+#   \item{PHQ}{precip of hottest quarter}
+#   \item{PCQ}{precip of coldest quarter}
 #' }
 #' @source \url{www.worldclim.org}
 #' @seealso \code{\link{climdat.fut}}, \code{\link{tree-data}}
