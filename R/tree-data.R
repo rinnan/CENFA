5b2816994472c26a042c6b5d551ff586c09468b0
rinnan <rinnan@u.washington.edu>
2018-01-30 09:34:38 -0800

    Package indexing.

--- R/tree-data.R
+++ R/tree-data.R
@@ -8,22 +8,9 @@
 #' @name tree-data
 #' @source \url{www.usgs.gov}
 #' @seealso \code{\link{climdat.hist}}, \code{\link{climdat.fut}}
+#' \describe{
+#'   \item{ABPR}{Historical range map for the noble fir (\emph{Abies procera})}
+#'   \item{QUGA}{Historical range map for the Oregon white oak (\emph{Quercus garryana})}
+#'   \item{SESE}{Historical range map for the coast redwood (\emph{Sequoia sempervirens})}
+#' }
 NULL
-
-#' @rdname tree-data
-#' @name ABPR
-#' Historical range map for the noble fir (\emph{Abies procera}).
-#'
-'ABPR'
-
-#' @rdname tree-data
-#' @name QUGA
-#' Historical range map for the garry oak (\emph{Quercus garryana}).
-#'
-'QUGA'
-
-#' @rdname tree-data
-#' @name SESE
-#' Historical range map for the coast redwood (\emph{Sequoia sempervirens}).
-#'
-'SESE'
