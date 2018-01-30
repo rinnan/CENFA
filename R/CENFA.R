2a816e281ae748e642f0ab1a06eafa3c6289de4b
rinnan <rinnan@u.washington.edu>
2018-01-30 14:33:43 -0800

    Startup message.

--- R/CENFA.R
+++ R/CENFA.R
@@ -45,7 +45,7 @@ NULL
 if(getRversion() >= "2.15.1")  utils::globalVariables(c("."))
 
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("500womenscientists.org")
+  packageStartupMessage("\nSupport women in science: 500womenscientists.org")
 }
 
 .onUnload <- function (libpath) {
